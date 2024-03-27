2f9297cb1285eb51fd1714aca3cc30799ad13ea4
temomaglakelidze <154346509+temomaglakelidze@users.noreply.github.com>
2024-03-27 19:22:14 +0400

    finished

---
+++ app.js
@@ -0,0 +1,19 @@
+var x = "word";
+
+console.log(x);
+
+let y = 2;
+
+console.log(y);
+
+const unit = "word1" + 2;
+
+console.log(unit);
+
+var realName = "temo";
+
+var age = 20;
+
+let someText = `hello my name is ${realName} and i am ${age} old`;
+
+console.log(someText);
